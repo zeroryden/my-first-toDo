e20f2f36494d7d3638e0695eb904e102d0f19023
zeroryden <jonyrob@gmail.com>
2021-04-04 19:41:56 -0300

    Terminada la 'logica' de la 'app'. Se pueden agregar y eliminar tareas

--- src/views/js.js
+++ src/views/js.js
@@ -4,25 +4,41 @@ let addBtn = document.getElementById("button-addon2")
         
 tasks.forEach(addWork);
         
-function addWork(t){
-    let li = document.createElement("li");
-    let text = document.createTextNode(t);
-    let att = document.createAttribute("class");
-    att.value= "list-group-item list-group-item-dark";
-    li.appendChild(text);
-    list.appendChild(li);
-    li.setAttributeNode(att);
-
-};
+function addWork(t,i){
+  let li = document.createElement("li");
+  let text = document.createTextNode(t);
+  let attLi = document.createAttribute("class");
+  let idLi = document.createAttribute("id");
+  let iconLi = document.createElement("link");
+  let attIcon = document.createAttribute("class");
+  let attValue = document.createAttribute("value");
+  
+  idLi.value= i;
+  attIcon.value= "fas fa-trash marIcon";
+  attLi.value= "list-group-item";
+  attValue.value = i
+  
+  li.appendChild(text);
+  li.appendChild(iconLi);
+  list.appendChild(li);
+  li.setAttributeNode(attLi);
+  li.setAttributeNode(idLi);
+  iconLi.setAttributeNode(attIcon);
+  iconLi.setAttributeNode(attValue);
+  iconLi.addEventListener("click", ()=>{
+    let idTask = document.getElementById(i);
+    let x = idTask.id;
+    list.removeChild(idTask);
+    tasks.splice(x,1);
+  });
+}
 
 addBtn.addEventListener("click", ()=>{
-    let task = document.getElementById("task").value;
-    if (task!=""){
-        tasks.push(task);
-        //console.log(tasks); 
-        addWork(task);
-    };          
-});
-
-//console.log(tasks[0]);
-//console.log(tasks[0].toString());
+  let task = document.getElementById("task").value;
+  let id;
+  if (task!=""){
+    id = tasks.length;
+    tasks.push(task);
+    addWork(task, id);
+  };          
+})
